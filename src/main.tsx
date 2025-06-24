b57f3919f9f3203371b409be3c80b975c0097c6f
crush <1351753317@qq.com>
2025-06-24 18:16:11 +0800

    feat: 添加托盘图标功能，支持右键菜单操作并优化应用标题

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,40 @@ import zhCN from "antd/locale/zh_CN";
 // for date-picker i18n
 import "dayjs/locale/zh-cn";
 
+import { show } from "@tauri-apps/api/app";
+import { TrayIcon } from "@tauri-apps/api/tray";
+import { defaultWindowIcon } from "@tauri-apps/api/app";
+import { Menu } from "@tauri-apps/api/menu";
+
+const menu = await Menu.new({
+  items: [
+    {
+      id: "quit",
+      text: "退出",
+    },
+  ],
+});
+
+const options = {
+  icon: await defaultWindowIcon(),
+  menu,
+  menuOnRightClick: true,
+  menuOnLeftClick: false,
+  action: (event: any) => {
+    switch (event.type) {
+      case "DoubleClick":
+        console.log("DoubleClick");
+        show();
+        break;
+
+      default:
+        break;
+    }
+  },
+};
+
+await TrayIcon.new(options);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ConfigProvider locale={zhCN}>
